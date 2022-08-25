1737ea9232a15d18496fe7cb09730929aa0cbba9
Arijit Basu <sayanarijit@gmail.com>
2022-08-25 11:45:54 +0530

    Add public keys

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,42 @@
 module.exports = function (eleventyConfig) {
-  const md = require('./eleventy/markdownIt');
-  eleventyConfig.setLibrary('md', md);
+  const md = require("./eleventy/markdownIt");
+  eleventyConfig.setLibrary("md", md);
 
   // filters
-  require('./eleventy/filters')(eleventyConfig, md);
+  require("./eleventy/filters")(eleventyConfig, md);
 
   // Shortcodes
-  require('./eleventy/shortcodes')(eleventyConfig, md);
+  require("./eleventy/shortcodes")(eleventyConfig, md);
 
-  const wm = require('./eleventy/webmentions');
-  eleventyConfig.addFilter('getMentionsForUrl', wm);
+  const wm = require("./eleventy/webmentions");
+  eleventyConfig.addFilter("getMentionsForUrl", wm);
 
-  eleventyConfig.addCollection('notes', function (collection) {
-    return collection.getFilteredByGlob(['src/site/notes/*.md']);
+  eleventyConfig.addCollection("notes", function (collection) {
+    return collection.getFilteredByGlob(["src/site/notes/*.md"]);
   });
 
-  eleventyConfig.addPassthroughCopy({ 'src/assets': 'assets' });
-  eleventyConfig.addPassthroughCopy({ 'src/site/keybase.txt': 'keybase.txt' });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/site/keybase.txt": "keybase.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/site/gpg.txt": "gpg.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/site/id_rsa.txt": "id_rsa.txt" });
+  eleventyConfig.addPassthroughCopy({
+    "src/site/id_ed25519.txt": "id_ed25519.txt",
+  });
   eleventyConfig.setUseGitIgnore(false);
 
-  const embedYouTube = require('eleventy-plugin-youtube-embed');
+  const embedYouTube = require("eleventy-plugin-youtube-embed");
   eleventyConfig.addPlugin(embedYouTube, { lite: true });
 
-  const pluginRss = require('@11ty/eleventy-plugin-rss');
+  const pluginRss = require("@11ty/eleventy-plugin-rss");
   eleventyConfig.addPlugin(pluginRss);
 
   return {
     dir: {
-      input: './src/site',
-      output: 'dist',
-      layouts: '../layouts',
-      includes: '../includes',
-      data: '../data',
+      input: "./src/site",
+      output: "dist",
+      layouts: "../layouts",
+      includes: "../includes",
+      data: "../data",
     },
     passthroughFileCopy: true,
   };
--- src/site/index.md
+++ src/site/index.md
@@ -10,8 +10,13 @@ I [write software][1], and take [notes][2], often publicly.
 
 Contact me via [Keybase][3], [Telegram][4], or [Email][5].
 
+Here are my public keys - [GPG][6], [RSA][7], [Ed25519][8].
+
 [1]: https://github.com/sayanarijit
 [2]: /notes
 [3]: https://keybase.io/sayanarijit
 [4]: https://t.me/sayanarijit
 [5]: mailto:hi@arijitbasu.in
+[6]: /gpg.txt
+[7]: /id_rsa.txt
+[8]: /id_ed25519.txt
